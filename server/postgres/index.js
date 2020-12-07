0beb630a1fc91c8c05599ca6833cb5db636e49b2
Victor Wong <victorw1992@gmail.com>
2020-12-07 11:58:09 -0800

    Added routes for postgres APIs

--- server/postgres/index.js
+++ server/postgres/index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+const Listings = require('./controller/listings');
+const Reservations = require('./controller/reservations');
 
 const app = express();
 
@@ -9,4 +11,10 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use('/listings/:id', express.static(path.join(__dirname, '..', 'client', 'dist')));
 
-app.get();
+app.get('/listings/:listingId/reservations/', Listings.getAll);
+app.get('/listings/:listingId/', Reservations.get);
+app.post('/listings/:listingId/reservation/', Listings.add);
+app.post('/listings/', Reservations.add);
+app.patch('/listings/:listingId/', Listings.update);
+app.delete('/reservation/:id', Reservations.delete);
+app.delete('/listing/:listingId', Listings.delete);
