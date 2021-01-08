935b5c92eb1110cd65909a7737171feedca83006
Victor Wong <victorw1992@gmail.com>
2021-01-08 09:23:06 -0800

    updated

--- controller/listings.js
+++ controller/listings.js
@@ -1,80 +1,94 @@
 const db = require('../server/client');
 
-// single query with for-loop
-// module.exports.get = (req, res) => {
-//   const id = req.params.listingId;
-//   db.pool.query(`
-//   SELECT * FROM bookings.listings l
-//   INNER JOIN bookings.reservations r ON l.id = r.listing_id
-//   INNER JOIN bookings.users u ON l.owner=u.id
-//   WHERE l.id = ${id}`, (err, data) => {
-//     if (err) {
-//       res.sendStatus(404);
-//     } else {
-//       const d = data.rows;
-//       const result = {
-//         id: d[0].id,
-//         owner: d[0].name,
-//         name: d[0].listing_name,
-//         reserved: [],
-//         fees: {
-//           pernight: d[0].feepernight,
-//           cleaning: d[0].feecleaning,
-//           service: d[0].feeservice,
-//         },
-//       };
-//       let dates;
-//       for (let i = 0; i < d.length; i += 1) {
-//         dates = [d[i].checkindate, d[i].checkoutdate];
-//         result.reserved.push(dates);
-//       }
-//       res.status(200).json(result);
-//     }
-//   });
-// };
-
+db.pool.on('error', (err, client) => {
+  console.log('Unexpected error on idle client', err);
+  process.exit(-1);
+});
 // single complex query no loops
 module.exports.get = (req, res) => {
   const id = req.params.listingId;
-  db.pool.query(`
-  SELECT l.*, u.name, JSON_agg(json_build_object('checkindate', r.checkindate, 'checkoutdate', r.checkoutdate)) AS reserved
-  FROM bookings.listings l
-  INNER JOIN bookings.reservations r ON l.listing_id = r.listing_id
-  INNER JOIN bookings.users u ON l.owner=u.user_id
-  WHERE l.listing_id = ${id} group by l.listing_id, u.name`, (err, data) => {
-    if (err) {
-      res.sendStatus(404);
-    } else {
-      const d = data.rows;
-      const result = {
-        id,
-        owner: d[0].name,
-        name: d[0].listing_name,
-        reserved: d[0].reserved,
-        fees: {
-          pernight: d[0].feepernight,
-          cleaning: d[0].feecleaning,
-          service: d[0].feeservice,
-        },
-      };
-      res.status(200).json(result);
-    }
+  db.pool.connect((err, client, done) => {
+    if (err) throw err;
+    client.query(`
+      SELECT l.*, u.name, JSON_agg(json_build_object('checkindate', r.checkindate, 'checkoutdate', r.checkoutdate)) AS reserved
+      FROM bookings.listings l
+      INNER JOIN bookings.reservations r ON l.listing_id = r.listing_id
+      INNER JOIN bookings.users u ON l.owner=u.user_id
+      WHERE l.listing_id = ${id} group by l.listing_id, u.name`, (err, data) => {
+      done();
+
+      if (err) {
+        res.sendStatus(404);
+      } else {
+        const d = data.rows;
+        const result = {
+          id,
+          owner: d[0].name,
+          name: d[0].listing_name,
+          reserved: d[0].reserved,
+          fees: {
+            pernight: d[0].feepernight,
+            cleaning: d[0].feecleaning,
+            service: d[0].feeservice,
+          },
+        };
+        res.status(200).json(result);
+      }
+    });
   });
 };
 
 module.exports.add = async (req, res) => {
   const json = JSON.stringify(req.body);
+  const client = await db.pool.connect();
   try {
-    await db.pool.query('BEGIN');
-    await db.pool.query(`
+    await client.query('BEGIN');
+    await client.query(`
     INSERT INTO bookings.listings(listing_name,maxstay,maxguests,feepernight,feecleaning,feeservice,owner)
     SELECT listing_name,maxstay,maxguests,feepernight,feecleaning,feeservice,owner
     FROM json_populate_record (NULL::bookings.listings, '${json}')`);
-    await db.pool.query('COMMIT');
+    await client.query('COMMIT');
     await res.sendStatus(202);
   } catch (e) {
-    await db.pool.query('ROLLBACK');
+    await client.query('ROLLBACK');
     console.log(e);
     res.sendStatus(404);
+  } finally {
+    client.release();
+  }
+};
+
+module.exports.update = async (req, res) => {
+  const keys = Object.keys(req.body);
+  const client = await db.pool.connect();
+  try {
+    await client.query('BEGIN');
+    await client.query(`
+    UPDATE bookings.listings SET (${keys}) = (SELECT ${keys} FROM json_populate_record (NULL::bookings.listings, '${JSON.stringify(req.body)}'))
+    WHERE listings_id = ${req.params.id}`);
+    await client.query('COMMIT');
+    await res.sendStatus(202);
+  } catch (e) {
+    await client.query('ROLLBACK');
+    console.log(e);
+    res.sendStatus(500);
+  } finally {
+    client.release();
+  }
+};
+
+module.exports.delete = async (req, res) => {
+  const client = await db.pool.connect();
+  try {
+    await client.query('BEGIN');
+    await client.query(`DELETE FROM bookings.listings WHERE listings_id = ${req.params.id}`);
+    await client.query('COMMIT');
+    await res.sendStatus(202);
+  } catch (e) {
+    await client.query('ROLLBACK');
+    console.log(e);
+    res.sendStatus(500);
+  } finally {
+    client.release();
   }
 };
--- server/postgres/index.js
+++ server/postgres/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 // const morgan = require('morgan');
 const Listings = require('../../controller/listings');
+// const db = require('../client');
 // const Reservations = require('../../controller/reservations');
 
 const port = 3002;
@@ -13,12 +14,12 @@ const app = express();
 app.use(express.json());
 app.use('/listings/:listingId', express.static(path.join(__dirname, '..', '..', 'client', 'dist')));
 
-app.get('/api/listings/:listingId/', Listings.get);
+app.get('/api/listings/:listingId', Listings.get);
 // app.get('/api/listings/:listingId/reservations/', Reservations.getAll);
 app.post('/api/listings/', Listings.add);
 // app.post('/api/listings/:listingId/reservation/', Reservations.add);
-// app.patch('/api/listings/:listingId/', Listings.update);
-// app.delete('/api/listing/:listingId', Listings.delete);
+app.patch('/api/listings/:listingId/', Listings.update);
+app.delete('/api/listing/:listingId', Listings.delete);
 // app.delete('/api/reservation/:id', Reservations.delete);
 
 app.listen(port, () => {
